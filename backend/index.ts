de5a57caf3b9039bb6033432081ccc21b6e18699
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-07-29 04:07:45 +0800

    fix the auth routes

--- backend/index.ts
+++ backend/index.ts
@@ -7,14 +7,13 @@ import authRoutes from "./routes/authRoutes";
 dotenv.config();
 
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-    res.send("API is running!");
-});
-
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
